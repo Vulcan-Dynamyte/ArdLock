ac962dd5759659f73ca001643e783c7ac6832452
Vulcan-Dynamyte <eeswardeep@gmail.com>
2016-01-13 22:16:26 +0530

    Final changes done

--- Keypad/examples/HelloKeypad/HelloKeypad.ino
+++ Keypad/examples/HelloKeypad/HelloKeypad.ino
@@ -17,8 +17,8 @@ char keys[ROWS][COLS] = {
   {'7','8','9'},
   {'*','0','#'}
 };
-byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {3, 4, 5, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {7, 8, 9}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
